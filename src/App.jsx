2ae19c6fd3f2bcc2a1eed1c94ef89f839bfcd1f6
DekuDz <boudjayanis@gmail.com>
2025-03-07 02:55:41 +0100

    Ajout d'une route protégée pour l'accès au tableau de bord administrateur

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Body from "./Body";
 import Admin from "./pages/Admin/Dashboard/Admin";
 import AdminAuth from "./pages/Admin/Auth/AdminAuth";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   console.log("App");
@@ -13,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Body />}/>
           <Route path="/Admin/logIn" element={<AdminAuth />} />
-          <Route path="/Admin/Dashboard" element={<Admin />} />
+          <Route path="/Admin/Dashboard" element={<ProtectedRoute element={<Admin />}></ProtectedRoute>} />
         </Routes>
       </Router>
     </>
---
+++ src/ProtectedRoute.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState, useCallback } from "react";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ element }) {
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+    const checkAdminAuth = useCallback(async () => {
+        const API_URL = import.meta.env.VITE_BACKEND_URI;
+
+        try {
+            const res = await fetch(`${API_URL}/api/check_admin_auth/`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+            });
+
+            if (res.ok) {
+                const data = await res.json();
+                setIsAuthenticated(data.authentication); 
+            } else {
+                setIsAuthenticated(false);
+            }
+        } catch (err) {
+            console.error("Error checking admin auth:", err);
+            setIsAuthenticated(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        checkAdminAuth();
+    }, [checkAdminAuth]);
+
+    if (isAuthenticated === null) {
+        return <div>Loading...</div>;
+    }
+
+    return isAuthenticated ? element : <Navigate to="/Admin/logIn" />;
+}
